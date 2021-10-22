9b30cb0431ea42bdda9c705b7dd8d82295ee64c9
pillow <fard.cca@gmail.com>
2021-10-22 22:58:59 +0200

    Fix the html and javascript

--- script/script.js
+++ script/script.js
@@ -1,20 +1,40 @@
+// define moment variables; global
 var today = moment().format("MMM Do, YYYY, h:mm:ss a");
 $("#currentDay").text(today);
-var currentHour = parseInt(moment().format("h"));
+var currentHourA = moment().format("h a");
 
-$(".save").on("click", function (event) {
-  var clickButtonTime = $(event.target).siblings().first();
-  var clickButtonTask = $(event.target).siblings().last();
+// define document variables
+var hourSlot = $(".row");
+var hourSlotChildren = hourSlot.children();
+console.log(typeof hourSlotChildren[1]);
 
-  var contentTime = parseInt(clickButtonTime.text());
-  if (contentTime < currentHour) {
-    console.log("akbariiii");
-    clickButtonTask.css({ "background-color": "#9fc777" });
-  } else if (contentTime === currentHour) {
-    console.log("piiiiiish");
-    clickButtonTask.css({ "background-color": "#1a1a1a" });
+// <>
+function changeHour() {
+  // change 12 Am/Pm to 24 hour
+  if (currentHourA.includes("pm")) {
+    var currentHour = parseInt(moment().format("h")) + 12;
   } else {
-    console.log("paaaaaanj");
-    clickButtonTask.css({ "background-color": "#630e3e" });
+    var currentHour = parseInt(moment().format("h"));
+  }
+  return currentHour;
+}
+
+// <>
+hourSlot.each(function () {
+  // change the color of rows here
+  var tableTime = parseInt($(this).attr("data-time"));
+  var currentTime = changeHour();
+  // var tableDescription = $(this).children[1];
+  console.log(tableTime);
+  console.log(currentTime);
+  if (tableTime < currentTime) {
+    console.log("before");
+    $(this).css("background-color", "#ede4e4");
+  } else if (tableTime === currentTime) {
+    console.log("now");
+    $(this).css("background-color", "#e0ffcf");
+  } else {
+    console.log("after");
+    $(this).css("background-color", "#cfd9ff");
   }
 });
